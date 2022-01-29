95d18960181e06f40583972b43d919cf5ddd6057
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-01-29 20:49:08 +0600

    first commit after 2 module

---
+++ practise.js
@@ -0,0 +1,40 @@
+function addNumber(number1, number2) {
+    return "Result is " + number1 + number2;
+   }
+   
+   var result = addNumber(5, 4);
+   console.log(typeof result);
+
+// function addTwoNumber(number1, number2) {
+//     console.log(number1, number2);
+//     var total = number1 * number2;
+//     return total;
+
+// }
+
+// var firstNumber = 40;
+// var secondNumber = 40;
+
+// var result = addTwoNumber(firstNumber, secondNumber);
+// console.log("result: ", result);
+
+
+
+
+
+
+
+
+
+
+// function bringSingara(taka){
+//     console.log("singara er jonno dise", taka);
+//     console.log("mama singara den.");
+//     var singaraPrice= 10;
+//     var singaraQuantity= taka/ singaraPrice;
+//     return singaraQuantity;
+// }
+
+// var money = 90;
+// var singara = bringSingara(money);
+// console.log("ei nen singara ", singara);
