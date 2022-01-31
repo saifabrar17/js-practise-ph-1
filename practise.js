44a818f02acf5749254a589fced0e27a69bfd02b
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-01-31 20:52:21 +0600

    smallest number

--- practise.js
+++ practise.js
@@ -1,13 +1,13 @@
-function largestNum(first, second, third){
-    if(first > second && first > third){
+function smallestNum(first, second, third){
+    if(first < second && first < third){
         return first;
     }
-    else if(second > first && second > third){
+    else if(second < first && second < third){
         return second;
     }
     else{
         return third;
     }
 }
-var values = largestNum(16, 8, 22);
-console.log("largest is", values);
+var values = smallestNum(-16, 8, 22);
+console.log("smallest is", values);
