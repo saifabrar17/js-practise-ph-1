796c4d203b848dd2c546e9d33eb93a74da41b3d6
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-01-31 21:14:07 +0600

    array total

--- practise.js
+++ practise.js
@@ -1,13 +1,25 @@
-function smallestNum(first, second, third){
-    if(first < second && first < third){
-        return first;
-    }
-    else if(second < first && second < third){
-        return second;
-    }
-    else{
-        return third;
-    }
+//METHOD 1
+const numbers = [12, 36, 45, 87, 96, 42];
+let sum = 0;
+
+for(let i = 0; i<numbers.length; i++){
+    let elements = numbers [i];
+    sum = sum + elements;
+    
 }
-var values = smallestNum(-16, 8, 22);
-console.log("smallest is", values);
+console.log(sum);
+
+
+// METHOD 2
+// function arrayTotal(numbers){
+//     let sum = 0;
+//     for(let i = 0; i<numbers.length; i++){
+//         let elements = numbers [i];
+//         sum = sum + elements;
+        
+//     }
+//     return sum;
+// }
+
+// let total = arrayTotal([10, 20, 30]);
+// console.log(total);
