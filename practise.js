6434fe4549586165de3852086dadd5135b47eebd
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-01-31 21:15:03 +0600

    array sum m2

--- practise.js
+++ practise.js
@@ -1,25 +1,25 @@
-//METHOD 1
-const numbers = [12, 36, 45, 87, 96, 42];
-let sum = 0;
+// //METHOD 1
+// const numbers = [12, 36, 45, 87, 96, 42];
+// let sum = 0;
 
-for(let i = 0; i<numbers.length; i++){
-    let elements = numbers [i];
-    sum = sum + elements;
+// for(let i = 0; i<numbers.length; i++){
+//     let elements = numbers [i];
+//     sum = sum + elements;
     
-}
-console.log(sum);
-
-
-// METHOD 2
-// function arrayTotal(numbers){
-//     let sum = 0;
-//     for(let i = 0; i<numbers.length; i++){
-//         let elements = numbers [i];
-//         sum = sum + elements;
-        
-//     }
-//     return sum;
 // }
+// console.log(sum);
 
-// let total = arrayTotal([10, 20, 30]);
-// console.log(total);
+
+METHOD 2
+function arrayTotal(numbers){
+    let sum = 0;
+    for(let i = 0; i<numbers.length; i++){
+        let elements = numbers [i];
+        sum = sum + elements;
+        
+    }
+    return sum;
+}
+
+let total = arrayTotal([10, 20, 30]);
+console.log(total);
